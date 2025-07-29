8cc7c52fbaa0f6c10066fd99decb51ce4237844d
Onwudeuju <sophyonwude291@gmail.com>
2025-07-29 03:23:30 -0700

    sixxx

--- routes/goalRoute.js
+++ routes/goalRoute.js
@@ -13,14 +13,16 @@ const router = express.Router();
 router.post("/", newGoal);
 // Create a route for getAllGoals
 router.get("/all", getAllGoals);
-// Create a route for getEachGoal......... anytime you are getting with id it should be dynamic
-router.get("/:id", eachGoal);
 // Create a Route for OngoingGoals
 router.get("/ongoing", onGoingGoals);
 // Create a new route for completedGoals
 router.get("/completed", completeGoals);
+// Create a route for getEachGoal......... anytime you are getting with id it should be dynamic
+router.get("/:id", eachGoal);
 // create a route for updatedProgress
 router.patch("/:id/progress", updateProgress);
 // create a route
-router.patch("/:id/delete", deleteGoal);
+router.delete("/:id/delete", deleteGoal);
+
 module.exports = router;
+// https://goal-backend-v8uh.onrender.com
